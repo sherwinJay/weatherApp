1d314c303e3aef6fd60d3ba21bd328cdddcb0ede
sherwinJay <sherwinsuva@gmail.com>
2017-07-02 09:05:22 +0800

    Update hello.js

--- js/hello.js
+++ js/hello.js
@@ -25,9 +25,20 @@ $(document).ready(function() {
            success: function(data){
              var forecast = '<div class="forecast cfix">';
              var weatherContainer = document.getElementById("weatherContainer");
+	     var currentCondition = data.query.results.channel.item.condition.text;
 	     var condition;
-	     var conditionImg;
+	     var conditionImg; 
+		     if(currentCondition.includes("Sunny")){
+		     conditionImg = '<img src="css/images/sunny.png" />';
+		     }else if(currentCondition.includes("Clear")){
+		     conditionImg = '<img src="css/images/nightr-icon.png" />';
+		     }else if(currentCondition.includes("Cloudy")){
+		     conditionImg = '<img src="css/images/cloudy.png" />';
+		     }else if(currentCondition.includes("Thunderstorms")){
+		     conditionImg = '<img src="css/images/thunderstorm.png" />';
+		     }
              $(".location").html(data.query.results.channel.location.city + ", " + data.query.results.channel.location.country);
+	     $(".imgContainer").html(conditionImg);
              $(".temp").html(data.query.results.channel.item.forecast[0].date + " Temp: " + data.query.results.channel.item.condition.temp + " C");
              $(".condition").html(data.query.results.channel.item.condition.text);
        //forecast div
