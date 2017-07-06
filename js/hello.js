6d9cf0d8f5d9efed911f294dad8d11c7e894971e
sherwinJay <sherwinsuva@gmail.com>
2017-07-06 14:18:33 +0800

    Update hello.js

--- js/hello.js
+++ js/hello.js
@@ -61,7 +61,7 @@ forecast += '</div>';
              
             $(".tempScale").on("click", "a" , function(e){
            e.preventDefault();
-             alert($(this).data("attr"));
+             alert($(this).attr("class"));
             /**$(".farenheit").closest(".containerWrapper").find(".temp").html( currentTemp * (9/5) + 32 );
              alert("Hello");**/
            });
