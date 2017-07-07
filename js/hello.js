6f0cb79d676384525318df3089f5c7272e175d85
sherwinJay <sherwinsuva@gmail.com>
2017-07-07 10:11:17 +0800

    Update hello.js

--- js/hello.js
+++ js/hello.js
@@ -5,7 +5,7 @@ function computeHeight(){
    return $(window).innerHeight();
   }  
   $("#weatherContainer").css({
-  height: windowHeight() + "px"
+  height: windowHeight() + "px",
   });
  console.log($("#weatherContainer").innerHeight);
  } 
