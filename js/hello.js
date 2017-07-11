2892c9ed7a18350e07aa9339c7618aebb1a267e6
sherwinJay <sherwinsuva@gmail.com>
2017-07-12 01:01:55 +0800

    Update hello.js

--- js/hello.js
+++ js/hello.js
@@ -78,11 +78,11 @@
                  $highlighted = $(".highlighted");
              $(".tempScale").on("click", ".farenheit" , function(e){  
              e.preventDefault();
-             $($highlighted).removeClass("highlighted");
+             $($highlighted).removeClass($highlighted);
              $(this).addClass("highlighted").closest(".containerWrapper").find(".temp").html(num.split(".")[1] ? parseFloat(num).toFixed(2) : num);
              }).on("click", ".celsius", function(e){
              e.preventDefault();
-             $($highlighted).removeClass("highlighted");
+             $($highlighted).removeClass($highlighted);
              $(this).addClass("highlighted").closest(".containerWrapper").find(".temp").html( currentTemp + "&#176;");
              });
            },
